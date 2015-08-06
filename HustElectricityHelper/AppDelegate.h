ab2b1f9dabb2e4f745b28068179bfbf75173e84f
MCLee <120710460@qq.com>
2015-08-06 11:02:38 +0800

    config CocoaPods & update .gitignore & config Minimum iOS Target 8.0

---
+++ HustElectricityHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HustElectricityHelper
+//
+//  Created by MC Lee on 15/8/6.
+//  Copyright (c) 2015年 ChiTu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HustElectricityHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HustElectricityHelper
+//
+//  Created by MC Lee on 15/8/6.
+//  Copyright (c) 2015年 ChiTu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
